741c3aa00094342d55abf9760df51534e713fc9e
Sebastien Dubois <sdubois@protonmail.com>
2016-09-23 20:09:26 +0300

    Remove German language

--- app/app.js
+++ app/app.js
@@ -115,7 +115,6 @@ if (!window.Intl) {
   }))
     .then(() => Promise.all([
       System.import('intl/locale-data/jsonp/en.js'),
-      System.import('intl/locale-data/jsonp/de.js'),
     ]))
     .then(() => render(translationMessages))
     .catch((err) => {
